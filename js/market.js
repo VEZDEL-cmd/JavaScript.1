7385edaa2bc1dc22e56d5a9666ebfd589f58bc14
Bohdan Vezdel <vezdel.studio@gmail.com>
2026-03-23 16:23:30 +0200

    mesage

---
+++ js/market.js
@@ -0,0 +1,33 @@
+
+
+let result = 5 + 5 + '5';
+console.log(result); // "105"
+console.log(typeof result); // string
+
+
+let email = "example@gmail.com";
+
+console.log(email.includes("@")); // true
+console.log(email.length); // кількість символів
+
+
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+
+let fullName = word1 + " " + word2 + " " + word3;
+fullName += " Bohdan";
+
+console.log(fullName); // My name is Bohdan
+
+
+let userName = "Vezhdel";
+let payment = 300.000;
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
+
+
+
+ 
+ 
